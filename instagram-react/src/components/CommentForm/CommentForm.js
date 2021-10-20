66184cda38508d7ee3ac8c65d001ac9dbe5af50b
Jonas Grossi <jonasgrossicc@gmail.com>
2021-10-19 22:55:56 -0300

    Styles_complete

--- instagram-react/src/components/CommentForm/CommentForm.js
+++ instagram-react/src/components/CommentForm/CommentForm.js
@@ -1,11 +1,17 @@
 //import React from "react";
 import "./styles.css";
-
+import {VscSmiley} from "react-icons/vsc";
 export default function CommentForm(props) {
   return (
-    <div className="component-form-container">
+    <div className="component-form-container" style={props.style}>
+      <VscSmiley size={30}/>
       <input className="component-form-input" placeholder={props.placeholder} />
-      <button className="component-form-button">Publicar</button>
+      <button
+        style={props.buttonColor ? { color: props.buttonColor } : {}}
+        className="component-form-button"
+      >
+        {props.buttonText}
+      </button>
     </div>
   );
 }
--- instagram-react/src/components/Post/Post.js
+++ instagram-react/src/components/Post/Post.js
@@ -3,15 +3,27 @@ import { CommentList } from "../Comments/Comments";
 import CommentForm from "../CommentForm/CommentForm";
 import "./styles.css"
 function Post(props) {
-  
-    return (
+
+  return (
     <div className="post-container">
-      
-      <Avatar name={props.data.userName}/>
-      <p className="post-message">{props && props.data ? props.data.message: "No message "}
+
+      <Avatar name={props.data.userName} />
+      <p className="post-message">{props && props.data ? props.data.message : "No message "}
+      </p>
+      {props && props.data && props.data.image ? (
+        <img style={{ marginTop: 10 }} src={props.data.image} width="100%"
+          alt="Imagem exemplo" />
+
+      ) : (
+
+        <p className="post-message">
+          {props && props.data ? props.data.message :
+            "No message"}
+
         </p>
+      )}
       <CommentList comments={props.data.comments} />
-      <CommentForm/>
+      <CommentForm buttonText="Comentar" placeholder="Escreva um comentário" />
     </div>
   );
 }
